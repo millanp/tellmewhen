58f11dd8771e12cd33a20e8c53a3bce59deec2c6
millanp <millan.philipose@gmail.com>
2017-04-11 18:50:05 -0700

    accept all strings in url

--- watcher/urls.py
+++ watcher/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^watch/(?P<msg>[\w-]+)$', views.StartWatchingView.as_view()),
+    url(ur'^watch/(?P<msg>.*)$', views.StartWatchingView.as_view()),
 ]
